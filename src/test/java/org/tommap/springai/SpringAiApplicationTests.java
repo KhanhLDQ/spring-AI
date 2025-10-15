a66ea6d6aef5964735d362315bbbca8291d5ffb6
Khanh Le <tommapcoder@gmail.com>
2025-10-15 22:41:04 +0700

    init project

---
+++ src/test/java/org/tommap/springai/SpringAiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tommap.springai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
